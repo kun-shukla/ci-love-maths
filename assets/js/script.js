e930d8a3de73a336871746546b41d69fd5f37988
Kunal S <kun.shukla@gmail.com>
2024-05-17 14:36:10 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello Worlds");
+function runGame() {}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubstractQuestion() {}
+
+function displayMultiplyQuestion() {}
