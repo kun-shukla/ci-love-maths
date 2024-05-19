b66e1953e714bd1fdf964da9396e7a94d02b34cd
Kunal S <kun.shukla@gmail.com>
2024-05-19 16:32:29 +0000

    Modify display divide question function such that the operands return a whole number after the division

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,6 +135,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion(operand1, operand2) {
+  operand1 = operand1 * operand2;
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "/";
